198e26e99e3d84a05ac9f27abd46a65292d77d61
mustanjid <mustanjid.se@gmail.com>
2025-03-30 03:46:48 +0600

    Route added to check vehicle activity

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Boat;
+use App\Models\Car;
+use App\Models\AmphibiousVehicle;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/vehicles', function(){
+	$car = new Car(['model' => 'Tesla', 'speed' => 150]);
+	$boat = new Boat(['model' => 'Yamaha', 'speed' => 80]);
+	$amphibious  = new AmphibiousVehicle(['model' => 'Hydra Spyder', 'speed' => 100]);
+	
+	return [
+		'Car Drive' => $car->move().", ".$car->drive(),
+		'Boat Sail' => $boat->move().", ".$boat->Sail(),
+		'Amphibious Drive' => $amphibious->move().", ".$amphibious->drive(),
+        'Amphibious Sail' => $amphibious->move().", ".$amphibious->sail(),
+	];
 });
