3eba2d2a954d42b5f915b4311c8aa5dd5036683d
mustanjid <mustanjid.se@gmail.com>
2025-04-08 17:58:00 +0600

    Method overloading route added

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
 use App\Services\Bird;
+use App\Services\BirdFlyService;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/method-overload', function(){
+	$birds = new BirdFlyService();
+	
+	return response()->json([
+		$birds->fly(), // no args
+		$birds->fly('fast'), // 1 args
+		$birds->fly('fast', 'north'), // 2 args
+        $birds->flying(), // method does not exist
+        $birds->fly('fast', 'north', 'extra'), // method does not exist 
+	]);
 });
