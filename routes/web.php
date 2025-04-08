306c0bb65816e3af20d8b6dd9d1ca10a7524fe9b
mustanjid <mustanjid.se@gmail.com>
2025-04-08 18:51:15 +0600

    method override route added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Bird;
+use App\Models\Eagle;
+use App\Models\Ostrich;
+use App\Models\Sparrow;
 use App\Services\BirdFlyService;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +21,20 @@ Route::get('/method-overload', function(){
         $birds->flying(), // method does not exist
         $birds->fly('fast', 'north', 'extra'), // method does not exist 
 	]);
+});
+
+Route::get('/method-override', function(){
+    $bird = [
+        new Bird(),
+        new Eagle(),
+        new Sparrow(),
+        new Ostrich(),
+    ];
+
+    $results = [];
+    foreach($bird as $b){
+        $results[] = $b->fly();
+    }
+
+	return response()->json($results);
 });
